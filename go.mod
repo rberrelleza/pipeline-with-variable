f07770f79e983d571fb703af30cb265fc220f0bb
Ramiro Berrelleza <ramiro@okteto.com>
2022-03-09 20:31:57 -0800

    Initial version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/okteto/pipeline-with-variables
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	fmt.Println("Starting hello-world server...")
+	http.HandleFunc("/", helloServer)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
+
+func helloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello %s!", os.Getenv("NAME"))
+}
